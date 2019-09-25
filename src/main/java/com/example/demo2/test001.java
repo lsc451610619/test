eb0bc4e1847938dd4c4aa2c767cf5b942c536586
LiShucheng <scli@pisx.com>
2019-09-25 14:37:21 +0800

    提交测试

---
+++ src/main/java/com/example/demo2/test001.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2019-2035 上海湃睿信息科技有限公司 (PISX). All Rights Reserved.
+ * <p>
+ * This software is the confidential and proprietary information of PISX
+ * and is su·bject to the terms of a software license agreement. You shall
+ * not disclose such confidential information and shall use it only in accordance
+ * with the terms of the license agreement.
+ */
+
+package com.example.demo2;
+
+/**
+ *
+ *@author: Li Shucheng
+ *
+ *@description:
+ *
+ *@date:2019/9/25
+ */
+
+public class test001 {
+}
